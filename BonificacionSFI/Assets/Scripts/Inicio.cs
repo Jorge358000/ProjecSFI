54ed00178688eec33573281bf80b8e4b7e6d764e
DiabloDa <jorge.callem@upb.edu.co>
2024-09-07 23:43:58 -0500

    Reemplanteamiento en algunas cosas, y adelanto del codigo

--- BonificacionSFI/Assets/Scripts/Inicio.cs
+++ BonificacionSFI/Assets/Scripts/Inicio.cs
@@ -3,22 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO.Ports;
 using TMPro;
+using UnityEngine.UI;
 
 public class Inicio : MonoBehaviour
 {
-
-    public TextMeshProUGUI myText;
-    private static int counter = 0;
+    private SerialPort _serialPort = new SerialPort();
+    private byte[] buffer = new byte[32];
+    public Text responseText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _serialPort.PortName = "COM3";
+        _serialPort.BaudRate = 115200;
+        _serialPort.DtrEnable = true;
+        _serialPort.Open();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        myText.text = counter.ToString();
-        counter++;
+
+    }
+    private void OnApplicationQuit()
+    {
+        if (_serialPort.IsOpen)
+        {
+            _serialPort.Close();
+        }
+
+    }
+
+    public void Enviar(string mensaje)
+    {
+        if (_serialPort.IsOpen)
+        {
+            _serialPort.WriteLine(mensaje);  
+            Debug.Log("Command sent: " + mensaje);
+        }
     }
 }
